9247bfc97c0db3e373e7957cb1359706451dfaee
Stephen Murray <Stephen@SMMBP.local>
2017-07-06 22:59:14 -0700

    Added struct for CLParams. Updated program logic.

--- dirsize.go
+++ dirsize.go
@@ -1,115 +1,120 @@
 package main
 
 import (
-	"fmt"
-	"os"
-	"path/filepath"
-	"flag"
+    "fmt"
+    "os"
+    "path/filepath"
+    "flag"
 )
 
 const (
-	BYTE     = 1.0
-	KILOBYTE = 1024.0 * BYTE
-	MEGABYTE = 1024.0 * KILOBYTE
-	GIGABYTE = 1024.0 * MEGABYTE
-	TERABYTE = 1024.0 * GIGABYTE
+    BYTE     = 1.0
+    KILOBYTE = 1024.0 * BYTE
+    MEGABYTE = 1024.0 * KILOBYTE
+    GIGABYTE = 1024.0 * MEGABYTE
+    TERABYTE = 1024.0 * GIGABYTE
 )
 
+type Params struct {
+    searchDir string
+    verboseOutput bool
+}
+
 func main() {
-	var size int64
-	var fileCount int64
-	var err error
 
-	dirToSize := flag.String("d", "", "Path you wish to return the size of.")
-	verbose := flag.Bool("v", false, "Output verbode directory size details.")
-	flag.Parse()
+    dirToSize := flag.String("d", "", "Path you wish to return the size of.")
+    verbose := flag.Bool("v", false, "Output verbose directory size details.")
+    flag.Parse()
 
-	searchDir := *dirToSize
-	verboseOutput := *verbose
+    clParam := Params{*dirToSize, *verbose}
 
-	if searchDir == "" {
-		// Quit out the program
-		flag.PrintDefaults()
-		os.Exit(0)
-	}
+    if clParam.searchDir == "" {
+        // Quit out the program
+        flag.PrintDefaults()
+        os.Exit(0)
+    }
 
-	if !DirExists(searchDir) {
-		fmt.Println("The supplied directory does not exist.")
-		fmt.Printf("Directory entered: %s", searchDir)
-	} else {
-		size, fileCount, err = DirSize(searchDir, verboseOutput)
-		if err != nil {
-			fmt.Println(err)
-		} else {
-			outputStats(size, fileCount)
-		}
-	}
+    if DirExists(clParam.searchDir) {
+        DirSize(clParam)
+    }
 
 }
 
 func DirExists(path string) (bool) {
-	_, err := os.Stat(path)
-	if os.IsNotExist(err) {
-		return false
-	} else {
-		return true
-	}
+    _, err := os.Stat(path)
+    if os.IsNotExist(err) {
+        fmt.Println("The supplied directory does not exist.")
+        fmt.Printf("Directory entered: %s", path)
+        return false
+    } else {
+        return true
+    }
 }
 
-func DirSize(path string, opt bool) (int64, int64, error) {
+func DirSize(clParams Params) {
     var totalSize int64
-    var dirSize int64
     var fileCount int64
-    m := make(map[string]int64)	// map to store directory sizes in
     var dirName string
+    var dirCount int64
 
-    err := filepath.Walk(path, func(p string, info os.FileInfo, err error) error {
+    dirMap := make(map[string]int64) // map to store directory sizes in
+
+    err := filepath.Walk(clParams.searchDir, func(pth string, info os.FileInfo, err error) error {
         if !info.IsDir() {
             totalSize += info.Size()
             fileCount += 1
-            dirSize += info.Size()
-            m[dirName] += dirSize
+            dirMap[dirName] += info.Size()
         } else {
-        	dirName = p
-        	_, ok := m[p]
-    		if  ok == false {
-    			m[p] = 0
-    		} 
+            dirCount += 1
+            dirName = pth
+            _, ok := dirMap[pth]
+            if  ok == false {
+                dirMap[pth] = 0
+            } 
         }
-        dirSize = 0
         return err
     })
 
-    if opt {
-    	for key, value := range m {
-    		fmt.Printf("Dir: %s \t Size: %d Bytes \n", key, value)
-		}
+    if err != nil {
+        fmt.Println(err)
+        os.Exit(0)
+    }   
+    
+    if clParams.verboseOutput {
+        for key, value := range dirMap {
+            dSize, sizeUnit := outputStats(value)
+            fmt.Printf("Dir: %s \t Size: %.2f %s \n", key, dSize, sizeUnit)
+        }
     }
-    return totalSize, fileCount, err
+
+    dSize, sizeUnit := outputStats(totalSize)
+    fmt.Printf("Dir size  : %.2f%s \n", dSize, sizeUnit)
+    fmt.Printf("Dir Count : %d\n", dirCount)
+    fmt.Printf("File count: %d\n", fileCount)
+
 }
 
-func outputStats(bytes int64, fcount int64) {
-	var value float64
-	var unit string
+func outputStats(bytes int64) (float64, string) {
+    var value float64
+    var unit string
 
-	switch {
-	case bytes >= TERABYTE:
-		unit = "TB"
-		value = float64(bytes) / TERABYTE
-	case bytes >= GIGABYTE:
-		unit = "GB"
-		value = float64(bytes) / GIGABYTE
-	case bytes >= MEGABYTE:
-		unit = "MB"
-		value = float64(bytes) / MEGABYTE
-	case bytes >= KILOBYTE:
-		unit = "KB"
-		value = float64(bytes) / KILOBYTE
-	default:
-		unit = "Bytes"
-		value = float64(bytes)
-	}
+    switch {
+    case bytes >= TERABYTE:
+        unit = "TB"
+        value = float64(bytes) / TERABYTE
+    case bytes >= GIGABYTE:
+        unit = "GB"
+        value = float64(bytes) / GIGABYTE
+    case bytes >= MEGABYTE:
+        unit = "MB"
+        value = float64(bytes) / MEGABYTE
+    case bytes >= KILOBYTE:
+        unit = "KB"
+        value = float64(bytes) / KILOBYTE
+    default:
+        unit = "Bytes"
+        value = float64(bytes)
+    }
 
-	fmt.Printf("Dir size: %.2f%s \n", value, unit)
-	fmt.Printf("File count: %d\n", fcount)
+    return value, unit
 }
